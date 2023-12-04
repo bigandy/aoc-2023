786b851a947852a98e3380b4ad0745f290b9136d
Andrew Hudson <ahudson@gmail.com>
2023-12-04 16:10:52 +0000

    day4: solve the second part

--- day-4/two.mjs
+++ day-4/two.mjs
@@ -1,9 +1,9 @@
-import { input } from "./data/dummy.mjs";
+import { input } from "./data/real.mjs";
 
 const guessesThatAreWinning = input
   .split("\n")
   .filter((s) => s !== "")
-  .slice(0, 1)
+  // .slice(0, 2)
   .map((row) => {
     const [_, nums] = row.split(":");
     const [winningNumbers, guesses] = nums.split("|");
@@ -15,7 +15,16 @@ const guessesThatAreWinning = input
     );
     return guessesThatAreWinning.length;
   });
-//   .reduce((acc, curr) => acc + getScoreFromCorrectGuesses(Number(curr)), 0);
-console.log({
-  guessesThatAreWinning,
+
+const answer = [...guessesThatAreWinning].map((_) => 1);
+
+answer.forEach((item, answerIndex) => {
+  const correctNumbers = guessesThatAreWinning[answerIndex];
+  for (let i = 1; i < correctNumbers + 1; i++) {
+    const solIndex = i + answerIndex;
+    answer[solIndex] = answer[solIndex] + item;
+  }
 });
+
+const solution = answer.reduce((prev, next) => prev + next, 0);
+console.log({ solution });
