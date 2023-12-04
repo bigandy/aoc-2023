83ae1c7544e1bb960bb2a3c288f5180b4c664f58
Andrew Hudson <ahudson@scalable.com>
2023-12-04 09:47:56 +0000

    day4: part 1

--- day-4/one.mjs
+++ day-4/one.mjs
@@ -1,4 +1,13 @@
-import { input } from "./data/dummy.mjs";
+import { input } from "./data/real.mjs";
+
+const getScoreFromCorrectGuesses = (correctGuesses) => {
+  if (correctGuesses === 0) return 0;
+  let result = 1;
+  for (let i = 1; i < correctGuesses; i++) {
+    result *= 2;
+  }
+  return result;
+};
 
 const guessesThatAreWinning = input
   .split("\n")
@@ -16,5 +25,8 @@ const guessesThatAreWinning = input
     );
     console.log(guessesThatAreWinning);
     return guessesThatAreWinning.length;
-  });
-console.log({ guessesThatAreWinning });
+  })
+  .reduce((acc, curr) => acc + getScoreFromCorrectGuesses(Number(curr)), 0);
+console.log({
+  guessesThatAreWinning,
+});
