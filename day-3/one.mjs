2ea461ebc19fe405cf3b2e6a8a355e99ae768286
Andrew Hudson <ahudson@scalable.com>
2023-12-06 11:39:28 +0000

    still trying

--- day-3/one.mjs
+++ day-3/one.mjs
@@ -1,91 +1,56 @@
 import { input } from "./data/dummy.mjs";
-import _ from "lodash";
 
+import _ from "lodash";
 const { range } = _;
 
-const rows = input.split("\n").filter((s) => s !== "");
-const stringRows = rows.join("");
-const rowLength = rows[2].length;
-console.log({ rowLength });
+const lines = input.split("\n").filter((s) => s !== "");
+const rowLength = lines[0].length;
+const rows = lines.join("");
 
-const numberRows = stringRows
-  .split(/(\d+)/) // split string on numbers
-  // .filter(Boolean) // remove empty array items
-  .filter((item) => item.match(/\d+/g)); // only keep the numbers
+const numbers = rows.match(/\d+/g);
 
-// get the sum of all the numbers
-const total = numberRows.reduce((prev, next) => prev + Number(next), 0);
+console.log({ rows });
 
-console.log(total);
+const positions = [];
+
+numbers.forEach((number) => {
+  const start = rows.indexOf(number);
+
+  const numbers = range(start, start + number.length);
+  positions.push({ numbers, number: Number(number) });
+});
 
 const getAdjacentCells = (cellIndex, rowLength) => {
-  const topLeft = cellIndex - rowLength - 1;
-  const topCenter = topLeft + 1;
-  const topRight = topCenter + 1;
-
-  const left = cellIndex - 1;
-  const right = cellIndex + 1;
-
-  const bottomLeft = cellIndex + rowLength - 1;
-  const bottomCenter = bottomLeft + 1;
-  const bottomRight = bottomCenter + 1;
-
-  return [
-    topLeft,
-    topCenter,
-    topRight,
-    left,
-    right,
-    bottomLeft,
-    bottomCenter,
-    bottomRight,
-  ].filter((num) => num >= 0);
+  // TODO!
 };
 
-/**
- * get the starting index and end index of a string within another string.
- */
-const getStartEnd = (searchString, fullString) => {
-  const start = fullString.indexOf(searchString);
-  const end = fullString.indexOf(searchString) + searchString.length - 1;
-
-  return range(start, end + 1);
-};
-
-const withoutTouchingSymbol = numberRows
-  // .slice(0, 3)
-  .filter((number) => {
-    const stringPositions = getStartEnd(number, stringRows);
-
-    // get all the string positions of the numbers
-    const surroundingLetters = stringPositions
-      .map((numberPosition) => {
-        const positions = getAdjacentCells(numberPosition, rowLength);
-
-        const letters = positions
-          .map((position) => {
-            return stringRows.charAt(position);
-          })
-          .join("");
-
-        return letters;
-      })
+const res = positions
+  // .slice(0, 1)
+  .filter(({ numbers, number }) => {
+    console.log({ numbers });
+    // console.log({ start, length, number });
+    //
+    const surroundingLetters = numbers
+      .map((index) => getAdjacentCells(index, rowLength))
+      .flat()
+      // .map((cell) => rows.charAt(cell))
       .join("");
-    console.log({ surroundingLetters, number });
+    // const values = cells.map((cell) => rows.charAt(cell));
+    console.log(surroundingLetters);
 
-    const regex = new RegExp("[^a-zA-Z0-9.]", "gi");
-    const matches = surroundingLetters.search(regex) === -1;
+    // const regex = new RegExp("[^a-zA-Z0-9.]", "gi");
+    // const matches = surroundingLetters.search(regex) === -1;
+    // // console.log({ matches });
 
-    return matches;
-  })
-  .reduce((prev, next) => prev + Number(next), 0);
+    // return matches;
+    // console.log({ number, values });
+  });
+// .reduce((acc, { number }) => acc + number, 0);
 
-console.log({ withoutTouchingSymbol, diff: total - withoutTouchingSymbol });
+// const totalNumbers = numbers.reduce((acc, num) => acc + Number(num), 0);
 
-// DID NOT COMPLETE THIS AS I THINK I APPROACHED IT IN THE WRONG WAY. TO BE CONTINUED!
-// x...x
-// x.0.x
-// x...x
-
-// console.log(getAdjacentCells(1, 5));
-// locations (7) = 1,2,3,6,8,11,12,13
+// console.log({
+//   res,
+//   numbers: totalNumbers,
+//   diff: totalNumbers - res,
+// });
