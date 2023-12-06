bdd1dc25af3f81eb9ab8bcb85e81bf33cc40be54
Andrew Hudson <ahudson@scalable.com>
2023-12-06 10:16:37 +0000

    day6: part2

--- day-6/two.mjs
+++ day-6/two.mjs
@@ -1,3 +1,23 @@
-import { input } from "./data/dummy.mjs";
+import { input } from "./data/real.mjs";
 
 const rows = input.split("\n").filter((s) => s !== "");
+
+const [timeRow, distanceRow] = rows;
+
+const time = Number(
+  timeRow.split(": ")[1].trim().split(" ").filter(Boolean).join("")
+);
+
+const distance = Number(
+  distanceRow.split(": ")[1].trim().split(" ").filter(Boolean).join("")
+);
+
+const out = [];
+for (let i = 0; i < time; i++) {
+  const distanceTravelled = i * (time - i);
+  if (distanceTravelled > distance) {
+    out.push(distanceTravelled);
+  }
+}
+
+console.log({ res: out.length });
